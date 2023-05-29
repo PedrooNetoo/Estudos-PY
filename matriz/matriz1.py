f53c2f32cca235a75ceb074bb1f494d71a6b3fcf
Pedro <131398818+PedrooNetoo@users.noreply.github.com>
2023-05-29 09:41:06 -0300

    Estudos Matriz PY

---
+++ matriz/matriz1.py
@@ -0,0 +1,10 @@
+numeros = [
+    [int(input("Digite a primeira idade: ")),int(input("Digite a segunda idade: "))],
+    [int(input("Digite a terceira idade: ")),int(input("Digite a quarta idade: "))]
+
+]
+
+print(numeros)
+
+
+
